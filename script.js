4a4530c4cfba415ebf1000a2a8efcbf8bf960410
Dilshan Senarath <134767047+SenarathDilshan@users.noreply.github.com>
2025-02-04 21:52:35 +0530

    modified js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ document.addEventListener('DOMContentLoaded', () => {
         userInput.value = '';
   
         try {
-            const response = await fetch('http://127.0.0.1:8080/chat', {
+            const response = await fetch('http://127.0.0.1:8000/chat', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
